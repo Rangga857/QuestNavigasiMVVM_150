8a97d4adc051823fb0cc3d43532c246e8fe2be81
Rangga857 <azharrangga28@gmail.com>
2024-11-21 15:50:21 +0700

    create reques untuk model dan response untuk view

--- app/src/main/java/com/example/mvvm/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/mvvm/viewmodel/SiswaViewModel.kt
@@ -8,5 +8,10 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
 class SiswaViewModel : ViewModel() {
+    //Request to model
+    private val _statusUI = MutableStateFlow(Datasiswa())
+    //Response to view
+    val statusUI : StateFlow<Datasiswa> = _statusUI.asStateFlow()
+
 
 }
