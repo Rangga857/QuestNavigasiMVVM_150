852f3fc6aaee3983ba8f699de9fae9e8b839477f
Rangga857 <azharrangga28@gmail.com>
2024-11-21 15:49:35 +0700

    create class SiswaViewModel in folder view model

---
+++ app/src/main/java/com/example/mvvm/viewmodel/SiswaViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.mvvm.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.mvvm.model.Datasiswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class SiswaViewModel : ViewModel() {
+
+}
